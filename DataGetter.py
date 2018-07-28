3fa50688753b7733e3073a38d8fcb58a5af4c804
petryshend <petryshend@gmail.com>
2018-07-28 12:54:58 +0300

    Implement data getter

---
+++ DataGetter.py
@@ -0,0 +1,20 @@
+import csv
+from datetime import datetime
+
+
+class DataGetter:
+    filename_format = 'resources/Environmental_Data_Deep_Moor_%d.txt'
+    date_format = "%Y_%m_%d %H:%M:%S"
+
+    def get_data(self, date_from, date_to):
+        res_data = []
+        for year in [2012, 2013, 2014, 2015]:
+            with open(self.filename_format % year, 'r') as csv_file:
+                reader = csv.reader(csv_file, delimiter='\t', quoting=csv.QUOTE_NONE)
+                next(reader, None)
+                for row in reader:
+                    date_row = datetime.strptime(row[0], self.date_format)
+                    if date_from.timestamp() < date_row.timestamp() < date_to.timestamp():
+                        res_data.append((row[0], row[2]))
+
+        return res_data
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('ready to go')
+from datetime import datetime
+from pprint import pprint
+from DataGetter import DataGetter
+
+if __name__ == '__main__':
+    date_from = datetime(2011, 1, 1, 0, 5, 30)
+    date_to = datetime(2016, 1, 1, 0, 30, 0)
+
+    data_getter = DataGetter()
+    res = data_getter.get_data(date_from, date_to)
+    pprint(len(res))
